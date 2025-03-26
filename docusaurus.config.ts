9e05e1d2f34fd8807c0afba17c15163821e23e40
sunginHwang <gommpo111@gmail.com>
2025-03-26 16:27:53 +0900

    feat: 기타 설정 세팅

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -2,40 +2,27 @@ import {themes as prismThemes} from 'prism-react-renderer';
 import type {Config} from '@docusaurus/types';
 import type * as Preset from '@docusaurus/preset-classic';
 
-// This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
 const config: Config = {
   title: 'Monolith',
   tagline: 'kakakostyle frontend common utility library',
   favicon: 'https://cf.res.s.zigzag.kr/favicons/zigzag/favicon.ico',
-
-  // Set the production url of your site here
-  url: 'https://github.com/croquiscom/monolith/',
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
+  url: 'https://kakaostyle.com',
   baseUrl: '/',
-
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'kakaostyle', // Usually your GitHub org/user name.
-  projectName: 'monoluth', // Usually your repo name.
-
+  organizationName: 'kakaostyle',
+  projectName: 'monoluth',
   onBrokenLinks: 'warn',
   onBrokenMarkdownLinks: 'warn',
-
-  // Even if you don't use internationalization, you can use this field to set
-  // useful metadata like html lang. For example, if your site is Chinese, you
-  // may want to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
+    defaultLocale: 'ko',
+    locales: ['ko'],
   },
   plugins: [
     [
       "docusaurus-plugin-typedoc",
       {
         outputFileStrategy:"members",
-        entryPoints: ["./src/components/*","./src/hooks/*"],
+        entryPoints: ["./src/components/*","./src/hooks/*","./src/utils/*"],
         tsconfig: "./tsconfig.json",
         plugin: ["./typedoc-plugin.mjs"],
         readme: "none",
@@ -63,8 +50,6 @@ const config: Config = {
         docs: {
           routeBasePath: '/docs', // 문서 경로를 /docs로 변경
           sidebarPath: './sidebars.ts',
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
           editUrl:
             'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
@@ -77,17 +62,11 @@ const config: Config = {
             type: ['rss', 'atom'],
             xslt: true,
           },
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
           editUrl:
             'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-          // Useful options to enforce blogging best practices
           onInlineTags: 'warn',
           onInlineAuthors: 'warn',
           onUntruncatedBlogPosts: 'warn',
-        },
-        theme: {
-          // customCss: './src/css/custom.css',
         },
       } satisfies Preset.Options,
     ],
